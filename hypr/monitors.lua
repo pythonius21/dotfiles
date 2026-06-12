935bc9802b16ebce8c1c186f31005910035ccac5
pythonius21 <silasgue24@gmail.com>
2026-06-12 20:33:35 +0200

    update

--- hypr/monitors.lua
+++
@@ -1,3 +0,0 @@
-hl.monitor({ output = "DP-1",     mode = "1920x1080@165",  position = "0x0",     scale = "1", bitdepth = 10 })
-hl.monitor({ output = "HDMI-A-2", mode = "3440x1440@100",  position = "0x-1440", scale = "1", bitdepth = 10 })
-hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@144",  position = "1920x0",  scale = "1", bitdepth = 10 })
